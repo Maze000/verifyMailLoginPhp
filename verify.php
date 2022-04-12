0ace08fc4ca8e8da0292a77e26543592978aa4f2
MAZE <72741681+Maze000@users.noreply.github.com>
2022-04-13 00:16:54 +1200

    Update verify.php

--- verify.php
+++ verify.php
@@ -18,7 +18,7 @@
           if($match > 0){
               $update = "UPDATE user SET active='1' WHERE mail='".$mail."' AND hash='".$hash."' AND active='0'" or die(mysql_error());
               mysqli_query($connection, $update);
-              echo "Your account has been activated, you can now <a style='font-weight:bold;color:#2BA6CB;' target='_blank' href='https://marionettegames.000webhostapp.com/verifyCorrection2/login.php'> Login </a>";
+              echo "Your account has been activated, you can now <a style='font-weight:bold;color:#2BA6CB;' target='_blank' href='https://yourWebSite/login.php'> Login </a>";
           }else{
               echo 'The url is invalid or you already have activated your account.';
           }
